79b7dccf02cc201c4129b8a3856d3328165ad7b3
WaysideTester <dylanmurray211@outlook.com>
2019-03-11 19:54:31 -0500

    Add project files.

---
+++ PlanetList/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PlanetList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
