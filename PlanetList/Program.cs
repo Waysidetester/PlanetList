2a456ba124cfb80fe231c37aeb50b1e43bd27933
WaysideTester <dylanmurray211@outlook.com>
2019-03-11 20:10:34 -0500

    inserting planets appropriately

--- PlanetList/Program.cs
+++ PlanetList/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace PlanetList
 {
@@ -6,7 +7,26 @@ namespace PlanetList
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            List<string> PlanetList = new List<string>
+            {
+                "Mercury",
+                "Mars"
+            };
+
+            PlanetList.Add("Jupiter");
+            PlanetList.Add("Saturn");
+
+            List<string> RemainingPlanets = new List<string>
+            {
+                "Uranus",
+                "Neptune"
+            };
+
+            PlanetList.AddRange(RemainingPlanets);
+            PlanetList.Insert(1, "Earth");
+            PlanetList.Insert(1, "Venus");
+
+            Console.WriteLine(PlanetList[2]);
         }
     }
 }
